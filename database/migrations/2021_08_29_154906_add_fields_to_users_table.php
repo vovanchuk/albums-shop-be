06100ed7bd677d56a05931bcf05e1f21d209532c
1ast <v.muzyka@mits.pl>
2021-12-13 22:22:23 +0100

    auth flow

--- database/migrations/2021_08_29_154906_add_fields_to_users_table.php
+++ database/migrations/2021_08_29_154906_add_fields_to_users_table.php
@@ -14,7 +14,7 @@ class AddFieldsToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('phone_number');
+            $table->string('phone_number')->nullable();
         });
     }
 
--- routes/api.php
+++ routes/api.php
@@ -15,15 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
 Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
 ], function ($router) {
-    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
